8ab1d007ad2341442c21e9228807fb95cbb04da9
oksmarty <hessdorf@ualberta.ca>
2017-03-01 19:03:35 -0700

    Adding a rooms collection.

--- app/client/index.tsx
+++ app/client/index.tsx
@@ -1,10 +1,10 @@
 import React = require("react");
 import ReactDOM = require("react-dom");
 
-import { Hello, VictoriaTest } from "./components/Hello";
+import { Hello } from "./components/Hello";
 //import { VictoriaTest } from "./components/Hello";
 
 Meteor.startup(() => ReactDOM.render(
-    <VictoriaTest compiler="TypeScript" framework="React" />,
+    <Hello compiler="TypeScript" framework="React" />,
     document.getElementById("root")
 ));
