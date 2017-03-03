376795b0c217a2cd8920ac44f3f277e4958f922f
Ross Anderson <ross.anderson@ualberta.ca>
2017-03-03 16:01:03 -0700

    Add routes mapping URL->page for empty pages

--- app/imports/startup/client/routes.jsx
+++ app/imports/startup/client/routes.jsx
@@ -14,21 +14,39 @@ import { Router, Route, browserHistory } from 'react-router';
 
 // route components
 import ParticipantContainer from '../../ui/containers/ParticipantContainer.jsx';
+import RoomsAvailable from '../../ui/pages/RoomsAvailable.jsx';
+import LoginPage from '../../ui/pages/LoginPage.jsx';
+import ParticipantGameScreen from '../../ui/pages/ParticipantGameScreen.jsx';
+import WaitingPage from '../../ui/pages/WaitingPage.jsx';
+import ParticipantResultsScreen from '../../ui/pages/ParticipantResultsScreen.jsx';
+import IndividualScoreboard from '../../ui/pages/IndividualScoreboard.jsx';
+
 import HostContainer from '../../ui/containers/HostContainer.jsx';
+import CreateARoom from '../../ui/pages/CreateARoom.jsx';
+import WelcomePage from '../../ui/pages/WelcomePage.jsx';
+import CollageScreen from '../../ui/pages/CollageScreen.jsx';
+import Scoreboard from '../../ui/pages/Scoreboard.jsx';
+
 import NullPage from '../../ui/pages/NullPage.jsx';
 
 // TODO i18n // i18n.setLocale('en');
 
+// TODO /rooms -> Rooms, and page Rooms figures out if there are rooms available or not
 export const renderRoutes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={ParticipantContainer}>
-      <Route path="rooms" component={NullPage} />
-      <Route path="join" component={NullPage} />
-      <Route path="play" component={NullPage} />
+      <Route path="rooms" component={RoomsAvailable} />
+      <Route path="join" component={LoginPage} />
+      <Route path="lobby" component={WaitingPage} />
+      <Route path="play" component={ParticipantGameScreen} />
+      <Route path="round-over" component={ParticipantResultsScreen} />
+      <Route path="game-over" component={IndividualScoreboard} />
     </Route>
     <Route path="host" component={HostContainer}>
-      <Route path="create" component={NullPage} />
-      <Route path="welcome" component={NullPage} />
+      <Route path="create" component={CreateARoom} />
+      <Route path="lobby" component={WelcomePage} />
+      <Route path="collage" component={CollageScreen} />
+      <Route path="game-over" component={Scoreboard} />
     </Route>
   </Router>
 );
