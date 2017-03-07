004848a713be10af424ad860e9848bbefe2efcec
Ross Anderson <ross.anderson@ualberta.ca>
2017-03-07 08:11:52 -0700

    ParticipantGameScreen: use className instead of class property

--- app/imports/ui/pages/participant/ParticipantGameScreen.jsx
+++ app/imports/ui/pages/participant/ParticipantGameScreen.jsx
@@ -26,7 +26,7 @@ export default class ParticipantGameScreen extends BaseComponent {
     }
 
     return (
-      <div class="game-screen">
+      <div className="game-screen">
         <p>The canvas will go here! And the prompt, and timer!</p>
         <p>You've received the name { Name }</p>
         <p>You're in room { Room.id }</p>
