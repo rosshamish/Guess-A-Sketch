17d2a4131972ba4cb9fbf301fbd1c080e0312203
Ross Anderson <ross.anderson@ualberta.ca>
2017-03-07 23:18:23 -0700

    Canvas is displayed with an ugly border

--- app/imports/ui/components/Canvas.jsx
+++ app/imports/ui/components/Canvas.jsx
@@ -3,18 +3,32 @@ import { _ } from 'meteor/underscore';
 import i18n from 'meteor/universe:i18n';
 import BaseComponent from './BaseComponent.jsx';
 
+import { fabric } from 'fabric';
+
 export default class Canvas extends BaseComponent {
   constructor(props) {
     super(props);
+    this.canvas = null;
+  }
+
+  componentDidMount() {
+    this.canvas = new fabric.Canvas('canvas');
+    console.log(this.canvas);
   }
 
   render() {
     const {
     } = this.props;
 
+    const style = {
+      border: '5px',
+      borderColor: 'black',
+      borderStyle: 'solid',
+    };
+
     return (
-      <div>
-        This is where the canvas would be displayed.
+      <div className="container">
+        <canvas id="canvas" style={style}/>
       </div>
     );
   }
