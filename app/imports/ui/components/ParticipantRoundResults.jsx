83dd7ba0d67f5eaa8db63462bff687a067e359eb
Ross Anderson <ross.anderson@ualberta.ca>
2017-03-10 14:48:48 -0700

    Fixup ParticipantRoundResults to use Sketches collection

--- app/imports/ui/components/ParticipantRoundResults.jsx
+++ app/imports/ui/components/ParticipantRoundResults.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Session } from 'meteor/session';
 import { _ } from 'meteor/underscore';
+
+import { PLAYER } from '/imports/api/session';
+import { Sketches } from '/imports/api/collections/sketches';
 
 import BaseComponent from './BaseComponent.jsx';
 import ErrorMessage from './ErrorMessage.jsx';
+
 
 export default class ParticipantRoundResults extends BaseComponent {
   constructor(props) {
@@ -16,8 +21,14 @@ export default class ParticipantRoundResults extends BaseComponent {
       round,
     } = this.props;
 
-    const currentPlayerSketches = _.filter(round.sketches, (sketch) => {
-      return sketch.player.name === sketch.player.name;
+    // TODO more efficient method than fetching ALL sketches and then
+    // filtering to the current plalyer's.
+    const currentPlayer = Session.get(PLAYER);
+    const sketches = _.map(round.sketches, (sketchID) => {
+      return Sketches.findOne({ _id: sketchID });
+    });
+    const currentPlayerSketches = _.filter(sketches, (sketch) => {
+      return sketch.player.name === currentPlayer.name;
     });
     if (currentPlayerSketches.length != 1) {
       console.error('Expected player to have exactly one sketch in the latest round.');
@@ -28,7 +39,7 @@ export default class ParticipantRoundResults extends BaseComponent {
 
     return (
       <div>
-        <img src={Session.get(SKETCH)} />
+        <img src={currentPlayerSketch.sketch} />
         <hr />
         <p>Scores: {currentPlayerSketch.scores}</p>
       </div>
