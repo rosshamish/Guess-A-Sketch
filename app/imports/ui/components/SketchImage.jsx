95490ae037985b5cc1f983eca76405e54b84c52b
Ross Anderson <ross.anderson@ualberta.ca>
2017-03-10 14:49:00 -0700

    CollageScreen displays SketchImage components for each sketch in the round

---
+++ app/imports/ui/components/SketchImage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { _ } from 'meteor/underscore';
+import i18n from 'meteor/universe:i18n';
+import BaseComponent from './BaseComponent.jsx';
+
+export default class SketchImage extends BaseComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { sketch } = this.props;
+
+    return (
+      <div className="sketch">
+        <img src={sketch.sketch} />
+      </div>
+    );
+  }
+}
+
+SketchImage.propTypes = {
+  sketch: React.PropTypes.object,
+};
--- app/imports/ui/pages/host/CollageScreen.jsx
+++ app/imports/ui/pages/host/CollageScreen.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { _ } from 'meteor/underscore';
 import BaseComponent from '../../components/BaseComponent.jsx';
 import { browserHistory } from 'react-router';
 import { Session } from 'meteor/session';
 
-import Canvas from '../../components/Collage.jsx';
-
 import { HOST_ROOM } from '/imports/api/session';
+import Canvas from '../../components/Collage.jsx';
+import ErrorMessage from '../../components/ErrorMessage.jsx';
+
+import { Sketches } from '/imports/api/collections/sketches';
+
 
 export default class CollageScreen extends BaseComponent {
   constructor(props) {
@@ -22,7 +26,6 @@ export default class CollageScreen extends BaseComponent {
     } else {
         browserHistory.push('/host/game-over');
     }
-
   }
 
   render() {
@@ -30,12 +33,27 @@ export default class CollageScreen extends BaseComponent {
       round,
     } = this.props;
 
+    if (!round) {
+      console.error('Round is undefined, cannot render.');
+      return <ErrorMessage />
+    }
+
+    const sketches = _.map(round.sketches, (sketchID) => {
+      return Sketches.findOne({ _id: sketchID });
+    });
+
+    const SketchComponents = _.map(sketches, (sketch) => {
+      return <SketchImage sketch={sketch} />
+    });
+
     return (
-      <div>
-      <p>Will display a collage of all drawings from the previous round!</p>
-      <form onSubmit={this.onNextRound}>
-        <button>Done</button>
-      </form>
+      <div className="collage">
+        <div className="sketches">
+          {SketchComponents}
+        </div>
+        <form onSubmit={this.onNextRound}>
+          <button>Done</button>
+        </form>
       </div>
     );
   }
