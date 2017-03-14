23460eb0f3a9464d0abe9abf2b4856c28a35dd7f
Ross Anderson <ross.anderson@ualberta.ca>
2017-03-14 11:17:46 -0600

    Delete unused Collage.jsx, DrawingResults.jsx, fix class name of HostEndGameScreen

--- app/imports/ui/components/HostEndGameScreen.jsx
+++ app/imports/ui/components/HostEndGameScreen.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import BaseComponent from './BaseComponent.jsx';
 
 
-export default class ParticipantEndGameScreen extends BaseComponent {
+export default class HostEndGameScreen extends BaseComponent {
   constructor(props) {
     super(props);
     this.state = {
