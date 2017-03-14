7a1ab1492e1063bd7da129e4b12f7a3141dca21f
oksmarty <hessdorf@ualberta.ca>
2017-03-14 12:49:01 -0600

    Basic EndGame Host Scoreboard skeleton completed.

--- app/imports/ui/components/HostEndGameScreen.jsx
+++ app/imports/ui/components/HostEndGameScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import BaseComponent from './BaseComponent.jsx';
-
+import RowComponent from './RowComponent.jsx';
 
 export default class HostEndGameScreen extends BaseComponent {
   constructor(props) {
@@ -13,10 +13,38 @@ export default class HostEndGameScreen extends BaseComponent {
     const {
       room,
     } = this.props;
+    
+    // compute a score for each player in the room based on their matching sketches
+    var scores = [];
+    players = room.players;
+    sketches = room.rounds.sketches;
+    for (var i in players) {
+      let score = 0;
+      let rank = parseInt(i) + 1;
+      for (var j in sketches){ // optimize this
+        if (sketches[j].player.name == players[i].name){
+          score += 1; // TO DO: make this a real judgement
+        }
+      }
+      scores[scores.length] = {rank:rank, name:players[i].name, score:score}
+    }
 
-    return (
-      <p>This will be the post-game overall scoreboard</p>
-    );
+    var renderScores = scores.map(function(row,index) {
+      return (
+        <RowComponent row = {row} />
+      );
+    });
+
+    return(<table>
+      <tbody>
+        <tr>
+          <th>Rank</th>
+          <th>Name</th>
+          <th>Score</th>
+        </tr>
+        {renderScores}
+      </tbody>
+      </table>);
   }
 }
 
