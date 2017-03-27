3bdcca6d15cfca96b30962c489978134ba8f96bb
oksmarty <hessdorf@ualberta.ca>
2017-03-27 17:33:50 -0600

    Still unsure of nicest way to display prompt on round results. Leaving it at this for now.

--- app/imports/ui/components/HostRoundResults.jsx
+++ app/imports/ui/components/HostRoundResults.jsx
@@ -58,7 +58,7 @@ export default class HostRoundResults extends BaseComponent {
             }/>
         </Segment>
         <Segment>
-          <Header as='h2'>Sketches from This Round (Prompt: &quot;{round.prompt}&quot;)</Header>
+          <Header as='h2'>Sketches from This Round (Prompt: {round.prompt})</Header>
           <StackGrid columnWidth={150}>
             {SketchComponents}
           </StackGrid>
