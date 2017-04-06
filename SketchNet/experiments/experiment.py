8fd4b0563d2e12300f90dbbed38d3ba1bccd3d0d
Anju Eappen <aeappen@ualberta.ca>
2017-04-06 13:24:39 -0600

    tensorboard wokring for accuracy

--- SketchNet/experiments/experiment.py
+++ SketchNet/experiments/experiment.py
@@ -28,7 +28,8 @@ class Experiment(object):
         # if summary_setup is None or not callable(summary_setup):
         #     raise NotImplementedError('Subclass must implement a summary setup method')
 
-        self.log_dir = log_dir or os.path.join('tmp', 'tensorflow')
+        self.log_dir = log_dir or os.path.join('/tmp', 'tensorflow')
+        print('Tensorboard summaries saved to {}'.format(self.log_dir))
 
         # Subclasses must provide these, both lists should look like:
         # - [
@@ -137,7 +138,7 @@ class Experiment(object):
                 self.label: training_batch[1],
                 self.keep_prob: self.model.keep_prob,
             })
-            if i % 10 == 0:
+            if i % 100 == 0:
                 train_accuracy,  summ = sess.run([self.model.accuracy, self.model.summary], {
                     self.image: training_batch[0],
                     self.label: training_batch[1],
@@ -149,6 +150,7 @@ class Experiment(object):
                 #     self.keep_prob: 1.0
                 # })
                 self.writer.add_summary(summ, i)
+                self.writer.flush()
 
                 print("step %d, training accuracy %g" % (i, train_accuracy))
                 test_batch = self._test_batch()
