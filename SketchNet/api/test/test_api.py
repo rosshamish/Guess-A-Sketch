c28c9a993b79a808ad8c0a933007a0e8d347d5fb
Anju Eappen <aeappen@ualberta.ca>
2017-04-12 22:32:36 -0600

    sketchnet tests - more

--- SketchNet/api/test/test_api.py
+++ SketchNet/api/test/test_api.py
@@ -1,8 +1,9 @@
 import collections
-from api import create_test_app
 import unittest
-from b64_img import B64_IMG
-from testing_props import APP_CREATION_ARGS
+
+from api.api import create_test_app
+from api.test.b64_img import *
+from api.test.testing_props import APP_CREATION_ARGS
 
 TEST_APP = create_test_app(APP_CREATION_ARGS).test_client()
 
@@ -31,7 +32,23 @@ class TestSketchNetAPI(unittest.TestCase):
         assert 'confidence' in resp_str
         assert 'label' in resp_str
 
-    def test_white_img(self):
+    def test_4_color_channels(self):
+        response = TEST_APP.post("/submit", data={'sketch': B64_4_CHANNELS},
+                                 headers={'content-md5': 'some hash'})
+
+        assert response.status == '200 OK'
+        assert response.content_type == 'application/json'
+        assert isinstance(response.response, collections.Iterable)
+
+        resp_str = list(response.response)[0]
+        assert 'confidence' in resp_str
+        assert 'label' in resp_str
+
+    def test_corrupt_img(self):
+        response = TEST_APP.post("/submit", data={'sketch': B64_IMG_EMPTY},
+                                 headers={'content-md5': 'some hash'})
+        assert response.status == '400 BAD REQUEST'
+
 
 if __name__ == '__main__':
     unittest.main()
